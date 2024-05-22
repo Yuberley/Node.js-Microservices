d018721c482fd610c2afc53ce1ee573658ba8dc7
Yuberley <yuberleyguerrero1999@gmail.com>
2024-05-21 22:22:01 -0500

    readiness for continuous deployment

--- products/src/config/index.js
+++ products/src/config/index.js
@@ -1,11 +1,5 @@
 const dotEnv = require("dotenv");
-
-if (process.env.NODE_ENV !== "prod") {
-    const configFile = `./.env.${process.env.NODE_ENV}`;
-    dotEnv.config({ path: configFile });
-} else {
-    dotEnv.config();
-}
+dotEnv.config();
 
 module.exports = {
     PORT: process.env.PORT,
