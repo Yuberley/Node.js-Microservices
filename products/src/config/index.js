4ac2cae8176b0d168805cfdb466cc385fc3d67b4
Yuberley <yuberleyguerrero1999@gmail.com>
2024-05-22 21:49:53 -0500

    chage file settings

--- products/src/config/index.js
+++ products/src/config/index.js
@@ -13,11 +13,11 @@ console.log(" ");
 
 module.exports = {
     PORT: process.env.PORT || 8002,
-    DB_URL: process.env.MONGODB_URI,
-    APP_SECRET: process.env.APP_SECRET,
+    DB_URL: process.env.MONGODB_URI || "mongodb+srv://yefersonguerrero:yefersonguerrero@efruvercluster.g37fx7f.mongodb.net/ms_products",
+    APP_SECRET: process.env.APP_SECRET || "ms_nodejs",
     BASE_URL: process.env.BASE_URL,
-    EXCHANGE_NAME: process.env.EXCHANGE_NAME,
-    MSG_QUEUE_URL: process.env.MSG_QUEUE_URL,
+    EXCHANGE_NAME: process.env.EXCHANGE_NAME || "ONLINE_STORE",
+    MSG_QUEUE_URL: process.env.MSG_QUEUE_URL || "amqps://euvxnjdk:muicNNpcjkF106zJijhPz1WyXCOzsmwQ@roedeer.rmq.cloudamqp.com/euvxnjdk",
     CUSTOMER_SERVICE: "customer_service",
     SHOPPING_SERVICE: "shopping_service",
 };
